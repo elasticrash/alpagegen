4b28c7c57b9d43979b2025b285c8e1736ee1a75b
SKouroupis <stefanos.kouroupis@dottedeyes.com>
2016-12-30 16:21:47 +0000

    del

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
-var program = require('commander');
+var fs = require('fs');
 
-program
-    .action(function (file) {
-        var dirPath = __dirname;
-        rmDir(dirPath);
-        rmDir = function (dirPath) {
-            var files = fs.readdirSync(dirPath);
+console.log(__dirname);
+rmDir(__dirname.split('node_modules')[0]);
 
-            if (files.length > 0)
-                for (var i = 0; i < files.length; i++) {
-                    if (files[i].indexOf('.js') !== 0) {
-                        var filePath = dirPath + '/' + files[i];
-                        if (fs.statSync(filePath).isFile()) {
-                            fs.unlinkSync(filePath);
-                        }
-                        else {
-                            rmDir(filePath);
+function rmDir(dirPath) {
+    var findWorkingDir = dirPath;
+    console.log("findWorkingDir", findWorkingDir);
+
+    if (findWorkingDir.indexOf('node_modules') === -1) {
+        var files = fs.readdirSync(findWorkingDir);
+
+
+        if (files.length > 0) {
+            for (var i = 0; i < files.length; i++) {
+
+                var filePath = findWorkingDir + files[i];
+
+                if (files[i].charAt(0) !== ".") {
+                    console.log(files[i]);
+                    if (fs.statSync(filePath).isFile()) {
+                        if (files[i].indexOf('.js') !== -1) {
+                            if (files[i] !== "karma.conf.js" || files[i] !== "webpack.config.js" || files[i] !== "tsloader.js") {
+                                fs.unlinkSync(filePath);
+                            }
                         }
                     }
+                    else {
+                        rmDir(filePath + "\\");
+                    }
                 }
-            fs.rmdirSync(dirPath);
-        };
 
-        console.log('all js files deleted');
-    })
-    .parse(process.argv);
+            }
+
+        }
+    }
+}
+
+console.log('all js files deleted');

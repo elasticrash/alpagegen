f67a6bb838e2d2748aabd17d650433aad53ae17f
stefano <s.kouroupis@gmail.com>
2017-01-11 14:35:29 +0000

    Update index.js

--- index.js
+++ index.js
@@ -11,40 +11,42 @@ function rmDir(dirPath) {
     if (findWorkingDir.indexOf('node_modules') === -1) {
         var files = fs.readdirSync(findWorkingDir);
 
+        // var dir = findWorkingDir.replace(appPath, appPath + "/test/mock/");
+        // if (!fs.existsSync(dir)) {
+        //     if (dir.indexOf('mock/test') === -1) {
+        //         console.log("create ", dir);
+        //         fs.mkdirSync(dir);
+        //     }
+        // }
 
         if (files.length > 0) {
             for (var i = 0; i < files.length; i++) {
 
                 var filePath = findWorkingDir + files[i];
 
-                if (files[i].charAt(0) !== ".") {
-                    if (fs.statSync(filePath).isFile()) {
-                        if (files[i].indexOf('.js') !== -1) {
-                            if (files[i] !== "karma.conf.js" &&
-                             files[i] !== "webpack.config.js" &&
-                              files[i] !== "tsloader.js" &&
-                               files[i].indexOf('.json') === -1 &&
-                                files[i].indexOf('.js.map') === -1) {
-                                if (files[i].indexOf('.spec.') === -1){
+                if (filePath.indexOf('test') === -1) {
+                    if (files[i].charAt(0) !== ".") {
+                        if (fs.statSync(filePath).isFile()) {
+                            if (files[i].indexOf('.js') !== -1) {
+                                if (files[i] !== "karma.conf.js" &&
+                                    files[i] !== "webpack.config.js" &&
+                                    files[i] !== "tsloader.js" &&
+                                    files[i].indexOf('.json') === -1 &&
+                                    files[i].indexOf('.js.map') === -1) {
                                     fs.unlinkSync(filePath);
-                                    console.log("deleted", files[i]);
-                                   } else {
-                                     fs.rename(filePath, appPath+"/test/mock/"+files[i]);
-                                     console.log("moved", files[i]);
-                                   }
+                                    //fs.rename(filePath, filePath.replace(appPath, appPath + "/test/mock/"));
+                                    console.log("delete", files[i]);
+                                }
                             }
                         }
-                    }
-                    else {
-                        rmDir(filePath + "/");
+                        else {
+                            rmDir(filePath + "/");
+                        }
                     }
                 }
-
             }
-
         }
     }
 }
 
-console.log('all js and js.map files deleted');
-console.log('all spec.js and spec.js.map files moved');
+console.log('all js files are deleted');

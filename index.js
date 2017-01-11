ac11d963c2416bf3439e9318a8845e8f806c738c
stefano <s.kouroupis@gmail.com>
2017-01-11 11:52:50 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var fs = require('fs');
 
 console.log(__dirname);
-rmDir(__dirname.split('node_modules')[0]);
+var appPath = __dirname.split('node_modules')[0];
+rmDir(appPath);
 
 function rmDir(dirPath) {
     var findWorkingDir = dirPath;
@@ -22,10 +23,15 @@ function rmDir(dirPath) {
                             if (files[i] !== "karma.conf.js" &&
                              files[i] !== "webpack.config.js" &&
                               files[i] !== "tsloader.js" &&
-                               files[i].indexOf('.json') === -1 
-                               && files[i].indexOf('.js.map') === -1) {
-                                fs.unlinkSync(filePath);
-                                console.log(files[i]);
+                               files[i].indexOf('.json') === -1 &&
+                                files[i].indexOf('.js.map') === -1) {
+                                if (files[i].indexOf('.spec.') === -1){
+                                    fs.unlinkSync(filePath);
+                                    console.log("deleted", files[i]);
+                                   } else {
+                                     fs.rename(filePath, appPath+"/test/mock/"+files[i]);
+                                     console.log("moved", files[i]);
+                                   }
                             }
                         }
                     }
@@ -40,4 +46,5 @@ function rmDir(dirPath) {
     }
 }
 
-console.log('all js files deleted');
+console.log('all js and js.map files deleted');
+console.log('all spec.js and spec.js.map files moved');
